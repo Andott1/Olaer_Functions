109061e96cde0dc0448ae2222619495e99fa7206
Andott1 <145018255+Andott1@users.noreply.github.com>
2023-12-12 10:16:56 +0800

    Olaer_Functions_Factor_Prime.py

--- Olaer_Functions_Factor_Prime.py
+++ Olaer_Functions_Factor_Prime.py
@@ -1,18 +1,18 @@
 def intro():
     print("\n------ CS112: COMPUTER PROGRAMMING 1 ------\n"
           "Created by: Kurt Andre L. Olaer\n")
-    print("Type [1] to find the smallest factor of n\n"
-          "Type [2] to find the prime numbers of range\n"
-          "Type [3] to terminate program\n")
+    print("Type [0] to terminate program\n"
+          "Type [1] to find the smallest factor of n\n"
+          "Type [2] to find the prime numbers of range\n")
     while True:
         try:
             selection = int(input("Choose an option: "))
-            if 1 <= selection <= 3:
+            if 0 <= selection <= 2:
                 break
             else:
-                print("Invalid input. Choose only between [1] or [2]\n")
+                print("Invalid input. Choose only [0], [1], or [2]\n")
         except ValueError:
-            print("Invalid input. Choose only between [1] or [2]\n")
+            print("Invalid input. Please enter a valid integer.\n")
 
     if selection == 1:
         opt1()
@@ -26,14 +26,14 @@ def intro():
 def input_again():
     while True:
         try:
-            response = int(input("[1] To input again, [2] To change function, [3] To terminate program\n"
+            response = int(input("[0] To terminate program, [1] To input again, [2] To change function.\n"
                                  "Choose an option: "))
-            if 1 <= response <= 3:
+            if 0 <= response <= 2:
                 break
             else:
-                print("Invalid input. Please choose between [1], [2], or [3].\n")
+                print("Invalid input. Choose only [0], [1], or [2]\n")
         except ValueError:
-            print("Invalid input. Please choose between [1], [2], or [3].\n")
+            print("Invalid input. Please enter a valid integer.\n")
 
     if response == 1:
         print("")
@@ -51,6 +51,9 @@ def opt1_input():
             num = int(input("Enter an integer greater than or equal to 2: "))
             if num >= 2:  # Check if the number is less than or greater than 2
                 return num
+            elif num == 0:
+                print("\n- Program Terminated -")
+                exit()
             else:
                 print("Invalid input. Enter a number greater than or equal to 2.\n")
         except ValueError:
@@ -58,11 +61,10 @@ def opt1_input():
 
 
 def opt1_factor(n):
-    # Check for divisibility from 2 to the square root of n
     for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
-            return i  # Found a factor
-    return n  # If no factor is found, n is a prime number
+            return i
+    return n
 
 
 def opt1():
@@ -78,17 +80,22 @@ def opt2_input():
         try:
             while True:
                 start_input = int(input("Enter a number [start]: "))
-                if start_input <= 0:
-                    print("Enter a non-negative number.\n")
+                if start_input < 0:
+                    print("Enter a non-negative integer.\n")
+                elif start_input == 0:
+                    print("\n- Program Terminated -")
+                    exit()
                 else:
                     break
             while True:
                 end_input = int(input("Enter a number [end]: "))
-                if end_input <= start_input:
+                if end_input == 0:
+                    print("\n- Program Terminated -")
+                    exit()
+                elif end_input <= start_input:
                     print("Enter a number greater than " + str(start_input) + ".\n")
                 else:
                     break
-
             return start_input, end_input
         except ValueError:
             print("Invalid input. Please enter a valid integer.\n")
@@ -115,8 +122,7 @@ def opt2_primes(start_value, end_value, primes_per_line=10, width=4):
     for i in range(0, len(primes_list), primes_per_line):
         line = primes_list[i:i + primes_per_line]
         formatted_line = [str(prime).rjust(width) for prime in line]
-        print(", ".join(formatted_line))
-
+        print(" | ".join(formatted_line))
     print("")
 
 
